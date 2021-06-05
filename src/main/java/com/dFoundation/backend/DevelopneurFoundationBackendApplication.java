18cc2e1acf0a848b8919f69fa7ad5d4538fbf695
shankyCdac1 <shankyCdac@gmail.com>
2021-06-05 17:46:57 +0530

    First commit

---
+++ src/main/java/com/dFoundation/backend/DevelopneurFoundationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dFoundation.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopneurFoundationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopneurFoundationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dFoundation/backend/DevelopneurFoundationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dFoundation.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopneurFoundationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
